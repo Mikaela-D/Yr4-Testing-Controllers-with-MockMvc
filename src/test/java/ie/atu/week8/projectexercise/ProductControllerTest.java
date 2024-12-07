d195e0b739869376043058e9f7b551ccc710bbee
Mikaela-D <G00401392@atu.ie>
2024-12-07 12:34:56 +0000

    Added testGetProductById test

--- src/test/java/ie/atu/week8/projectexercise/ProductControllerTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductControllerTest.java
@@ -2,10 +2,25 @@ package ie.atu.week8.projectexercise;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
 
     @BeforeEach
     void setUp() {
@@ -16,7 +31,13 @@ class ProductControllerTest {
     }
 
     @Test
-    void testGetProductById() {
+    void testGetProductById() throws Exception {
+        Product product = new Product(1L, "Pot", "10 litres", 30);
+        when(productService.getProductById(1L)).thenReturn(Optional.of(product));
+        mockMvc.perform(get("/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Pot"));
+
     }
 
     @Test
